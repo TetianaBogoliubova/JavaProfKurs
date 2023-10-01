06639eeb8d45c556555d85aeebe9ac18d054e9d0
Bogoliubova.Tetiana <bogol.tatyana@gmail.com>
2023-10-01 14:14:17 +0200

    HW_27.09.23_Roulette

--- src/main/java/homework_27_09_23_roulette/Player.java
+++ src/main/java/homework_27_09_23_roulette/Player.java
@@ -2,32 +2,6 @@ package homework_27_09_23_roulette;
 
 public class Player {
 
-
-
-//    private int numSerios;//порядковый номер серии, который вводит игрок
-//    private int numComplit;//порядковый номер комплита, который вводит игрок
-//    private int numFromComplit;// число, выбранное игроком в пределах комплита
-//    //private int minGitonsForBet;
-//
-//    public int getNumSerios() {
-//        return numSerios;
-//    }
-//
-//    public int getNumComplit() {
-//        return numComplit;
-//    }
-//
-//    public int getNumFromComplit() {
-//        return numFromComplit;
-//    }
-
-
-
-
-
-
-
-
     private int inputNumber; //номер, который вводит игрок
     private String inputSection;// секция (odd/even), которую вводит игрок
     private int inputSerios;// порядковый номер серии, который вводит игрок
